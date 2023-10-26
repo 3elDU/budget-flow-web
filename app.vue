6ba5dac131118c4aa44cf2466b7356e20e73c222
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-10-26 13:46:19 +0300

    [BFA-12] Initial setup

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>
+      Hello, world!
+    </h1>
+  </div>
+</template>
+
+<style>
+div {
+  width: 100dvw;
+  height: 100dvh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bolder;
+  font-family: monospace;
+  font-size: 3rem;
+}
+</style>
